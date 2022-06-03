65d9b3dc5c4ae8ee8ee947803ffc3b51a09ef547
Matthew Chivers <matthew.j.chivers@gmail.com>
2022-06-03 19:16:49 -0400

    Update latest location data

--- fantasysixtysix/trip.js
+++ fantasysixtysix/trip.js
@@ -5,11 +5,12 @@ import { locations } from "./locations.js";
 import { getTilesets, buildPath } from "./utils.js";
 
 const tilesets = getTilesets();
-
+const lastLocation = locations[locations.length - 1];
+console.log(lastLocation)
 const map = L.map("map-container", {
   zoomControl: false,
   layers: Object.values(tilesets),
-}).setView([47.22797, -79.9651], 13);
+}).setView([lastLocation.lat, lastLocation.lng], 11);
 
 const [markers, path] = buildPath(locations);
 markers.map((marker) => marker.addTo(map));
@@ -17,6 +18,7 @@ path.addTo(map);
 
 L.control.zoom({ position: "bottomright" }).addTo(map);
 L.control.layers(tilesets).addTo(map);
+tilesets["Street"].addTo(map);
 L.control
   .slider({
     position: "topleft",
@@ -28,3 +30,4 @@ L.control
     map,
   })
   .addTo(map);
+
