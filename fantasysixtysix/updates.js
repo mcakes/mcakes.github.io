7017c44a0612c5618d742aa70c27d9a01792134f
Matthew Chivers <matthew.j.chivers@gmail.com>
2022-06-14 00:37:21 -0400

    Updates

--- fantasysixtysix/updates.js
+++ fantasysixtysix/updates.js
@@ -72,22 +72,135 @@ export const updates = [
 `
   },
   {
-    timestamp: "2022-06-05",
-    text: `
-    <h2>Week 3<h2>
-    <p>A brutally hard week of traveling! </p>
-    <p>
+    timestamp: "2022-05-28",
+    text: `<h2>Day 15</h2>
       Day 15 saw Dan & I climb cliff on Marina L for view over it and Smoothwater. 
-      Park rangers portaged right through our camp without checking for the permits we didn’t have! YEEHAW!!
-    </p>
-    <p> Day 16 I left Dan and moved south. 10km of walking, 6km hauling 55, 65 & 75 lb loads! Buggiest camp ever!  </p>
-    <p> Day 17 was steep uphill on trapper trail. 4 loads!!! Exhausting!  <p
-    <p> Day 18 took 8 hrs to move 3km! 4 very steep carries on 2 portages. Made it to Broken Pelvis Lake and took a couple days off.  </p>
-    <p> Day 19 in camp with rain & repairs. </p>
-    <p> Day 20 hiked trail along Pilgrim Cr past Scarecrow Cut. Had conversation with a beaver. Beaverspeak sounds like mournful whalesong! </p>
-    <p> Day 21 headed for Chivers L but only got to Horseshoe L. 9 hrs to go 3km! Afternoon t-storm delayed progress. Another big one in evening. </p>
-    <p> Day 22 made it to Chivers Island! Showers & sun all day. Saw a full rainbow over the Mtn across the lake with both ends touching down IN the lake! Stunning! </p>
-    <p> Day 23 relaxing in camp. Probably go for a hike this aft.  </p>
+      Park rangers portaged right through our camp without checking for the permits we didn't have! YEEHAW!!
     `
+  },
+  {
+    timestamp: "2022-05-29",
+    text: `<h2>Day 16</h2>
+      I left Dan and moved south. 10km of walking, 6km hauling 55, 65 & 75 lb loads! Buggiest camp ever!
+    `
+  },
+  {
+    timestamp: "2022-05-30",
+    text: `<h2>Day 17</h2>
+    Was steep uphill on trapper trail. 4 loads!!! Exhausting!  
+    `
+  },
+  {
+    timestamp: "2022-05-31",
+    text: `<h2>Day 18</h2>
+    Took 8 hrs to move 3km! 4 very steep carries on 2 portages. Made it to Broken Pelvis Lake and took a couple days off. 
+    `
+  },
+  {
+    timestamp: "2022-06-01",
+    text: `<h2>Day 19</h2>
+    In camp with rain & repairs. 
+    `
+  },
+  {
+    timestamp: "2022-06-02",
+    text: `<h2>Day 20</h2>
+    Hiked trail along Pilgrim Cr past Scarecrow Cut. Had conversation with a beaver. Beaverspeak sounds like mournful whalesong!
+    `
+  },
+  {
+    timestamp: "2022-06-03",
+    text: `<h2>Day 21</h2>
+    Headed for Chivers L but only got to Horseshoe L. 9 hrs to go 3km! Afternoon t-storm delayed progress. Another big one in evening. 
+    `
+  },
+  {
+    timestamp: "2022-06-04",
+    text: `<h2>Day 22</h2>
+    Made it to Chivers Island! Showers & sun all day. Saw a full rainbow over the Mtn across the lake with both ends touching down IN the lake! Stunning!
+    `
+  },
+  {
+    timestamp: "2022-06-05",
+    text: `<h2>Day 23</h2>
+    Relaxing in camp. Probably go for a hike this aft. 
+    <br />
+    After writing & sending Week 3 updates went for a hike to locate a cliff top Lookout I'd noticed a few years ago from the water but was unable to find it. 
+    On the way noticed an odd sensation in my right foot that later became obvious as a symptom of a building sciatica attack.
+    `
+  },
+  {
+    timestamp: "2022-06-06",
+    text: `<h2>Day 24</h2>
+    A rain day. Rearranged my gazebo, made a few repairs & finished my last e-cigarette. Back/leg pain increasing.
+    `
+  },
+  {
+    timestamp: "2022-06-07",
+    text: `<h2>Day 25</h2>
+    Rained in the morning & in aft scouted the next portage & found it in fine shape. Sciatica really kicking in!
+    `
+  },
+  {
+    timestamp: "2022-06-08",
+    text: `<h2>Day 26</h2>
+    Very restless sleep with back pain. Moved to Bluesucker Lake. 11 hrs to make 11 km w/ 5 portages. 
+    Had to cut & flag a 500m portage & last one was just a ridiculous bushcrash. 
+    In camp late, supper at 10 & forgot to send location updates. My location was: 47.16374, -80.59473. 
+    `
+  },
+  {
+    timestamp: "2022-06-09",
+    text: `<h2>Day 27</h2>
+    Moved to Talking Falls, 4km with one portage, 135m took over an hour! Slipped & gashed my arm. T-storm hit just after setting up bug shelter.
+    `
+  },
+  {
+    timestamp: "2022-06-10",
+    text: `<h2>Day 28</h2>
+    Restless night. Took over 5 hours to pack up & go. 7 hrs to make 8 km w/ 4 short portages. 
+    Can carry pack loads ok but canoe is agony. Walking upright near impossible. 
+    Setting up camp agony, specially overhead work stringing tarp ropes. Supper after 10 again & forgot to send locations.
+    `
+  },
+  {
+    timestamp: "2022-06-11",
+    text: `<h2>Day 29</h2>
+    Hard to get comfortable sleep. Answered messages from last 2 days. Overhauled malfunctioning stove. 
+    Saw 1st human since leaving Dan Day 16. Dan from Pittsburgh on a solo trip. 
+    Resting today and if back not better will arrange a partial extraction at Twinkle L. 
+    (a few hours travel and 3 short portages from here) & go to Alex's on Obabika to recoup. 
+    Have been on ibuprofen since day 24 & periodic 1/4 Perc when moving 
+    (might explain some of the tumbles I've taken).]
+    ` 
   }
 ]
+
+  // }
+// Week 4 To sum it all up? Pure screaming’ agony!
+// Day 23 After writing & sending Week 3 updates went for a hike to locate a cliff top Lookout I’d noticed a few years ago from the water but was unable to find it. On the way noticed an odd sensation in my right foot that later became obvious as a symptom of a building sciatica attack.
+// Day 24 a rain day. Rearranged my gazebo, made a few repairs & finished my last e-cigarette. Back/leg pain increasing.
+// Day 25 rained in the morning & in aft scouted the next portage & found it in fine shape. Sciatica really kicking in!
+// Day 26 Very restless sleep with back pain. Moved to Bluesucker Lake. 11 hrs to make 11 km w/ 5 portages. Had to cut & flag a 500m portage & last one was just a ridiculous bushcrash. In camp late, supper at 10 & forgot to send location updates. My location was: 47.16374, -80.59473. 
+// Day 27 moved to Talking Falls, 4km with one portage, 135m took over an hour! Slipped & gashed my arm. T-storm hit just after setting up bug shelter.
+// Day 28 restless night. Took over 5 hours to pack up & go. 7 hrs to make 8 km w/ 4 short portages. Can carry pack loads ok but canoe is agony. Walking upright near impossible. Setting up camp agony, specially overhead work stringing tarp ropes. Supper after 10 again & forgot to send locations.
+// Day 29 hard to get comfortable sleep. Answered messages from last 2 days. Overhauled malfunctioning stove. Saw 1st human since leaving Dan Day 16. Dan from Pittsburgh on a solo trip. Resting today and if back not better will arrange a partial extraction at Twinkle L. (a few hours travel and 3 short portages from here) & go to Alex’s on Obabika to recoup. Have been on ibuprofen since day 24 & periodic 1/4 Perc when moving (might explain some of the tumbles I’ve taken).]
+
+  // {
+  //   timestamp: "2022-06-05",
+  //   text: `
+  //   <h2>Week 3<h2>
+  //   <p>A brutally hard week of traveling! </p>
+  //   <p>
+  //     Day 15 saw Dan & I climb cliff on Marina L for view over it and Smoothwater. 
+  //     Park rangers portaged right through our camp without checking for the permits we didn’t have! YEEHAW!!
+  //   </p>
+  //   <p> Day 16 I left Dan and moved south. 10km of walking, 6km hauling 55, 65 & 75 lb loads! Buggiest camp ever!  </p>
+  //   <p> Day 17 was steep uphill on trapper trail. 4 loads!!! Exhausting!  <p
+  //   <p> Day 18 took 8 hrs to move 3km! 4 very steep carries on 2 portages. Made it to Broken Pelvis Lake and took a couple days off.  </p>
+  //   <p> Day 19 in camp with rain & repairs. </p>
+  //   <p> Day 20 hiked trail along Pilgrim Cr past Scarecrow Cut. Had conversation with a beaver. Beaverspeak sounds like mournful whalesong! </p>
+  //   <p> Day 21 headed for Chivers L but only got to Horseshoe L. 9 hrs to go 3km! Afternoon t-storm delayed progress. Another big one in evening. </p>
+  //   <p> Day 22 made it to Chivers Island! Showers & sun all day. Saw a full rainbow over the Mtn across the lake with both ends touching down IN the lake! Stunning! </p>
+  //   <p> Day 23 relaxing in camp. Probably go for a hike this aft.  </p>
+  //   `
